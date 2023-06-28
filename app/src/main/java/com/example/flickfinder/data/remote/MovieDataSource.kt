f9774e658f1bb8cfd6b5274870d0f0ee2684b0b0
Fabian <fabian_carrazan@hotmail.com>
2023-06-28 16:33:40 -0300

    Data Layer

--- app/src/main/java/com/example/flickfinder/data/remote/MovieDataSource.kt
+++ app/src/main/java/com/example/flickfinder/data/remote/MovieDataSource.kt
@@ -1,4 +1,18 @@
 package com.example.flickfinder.data.remote
 
+import com.example.flickfinder.data.model.MovieList
+
 class MovieDataSource {
+
+    fun getUpcomingMovies(): MovieList {
+        return MovieList()
+    }
+
+    fun getTopRatedMovies(): MovieList {
+        return MovieList()
+    }
+
+    fun getPopularMovies(): MovieList {
+        return MovieList()
+    }
 }
