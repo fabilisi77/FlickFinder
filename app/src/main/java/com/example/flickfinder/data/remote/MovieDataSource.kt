d2bc1a90bca19e5fcc59f5b22ca46c2ef219a902
Fabian <fabian_carrazan@hotmail.com>
2023-06-29 10:48:00 -0300

    Data Source

--- app/src/main/java/com/example/flickfinder/data/remote/MovieDataSource.kt
+++ app/src/main/java/com/example/flickfinder/data/remote/MovieDataSource.kt
@@ -1,18 +1,20 @@
 package com.example.flickfinder.data.remote
 
 import com.example.flickfinder.data.model.MovieList
+import com.example.flickfinder.repository.WebService
+import com.example.flickfinder.utils.AppConstants
 
-class MovieDataSource {
+class MovieDataSource(private val webService: WebService) {
 
-    fun getUpcomingMovies(): MovieList {
-        return MovieList()
+    suspend fun getUpcomingMovies(): MovieList {
+        return webService.getUpcomingMovies(AppConstants.API_KEY)
     }
 
-    fun getTopRatedMovies(): MovieList {
-        return MovieList()
+    suspend fun getTopRatedMovies(): MovieList {
+        return webService.getTopRatedMovies(AppConstants.API_KEY)
     }
 
-    fun getPopularMovies(): MovieList {
-        return MovieList()
+    suspend fun getPopularMovies(): MovieList {
+        return webService.getPopularMovies(AppConstants.API_KEY)
     }
 }
