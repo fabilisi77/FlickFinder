1252e5c7546e976e85773f183ff184b8695f9da3
FabiDevs.77 <55720965+fabilisi77@users.noreply.github.com>
2023-06-30 10:41:56 -0300

    Revert "Feature UI layer"

--- app/src/main/java/com/example/flickfinder/MainActivity.kt
+++ app/src/main/java/com/example/flickfinder/MainActivity.kt
@@ -1,8 +1,7 @@
-package com.example.flickfinder.ui
+package com.example.flickfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.flickfinder.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/flickfinder/core/Resource.kt
+++
@@ -1,9 +0,0 @@
-package com.example.flickfinder.core
-
-sealed class Resource<out T>{
-    class Loading<out T>: Resource<T>()
-    data class Succes<out T> (val data:T): Resource<T>()
-    data class Failure (val exception:Exception): Resource<Nothing>()
-
-
-}
