43cd7c8e2c645f74093d478713d10b9d9d5a1a04
Fabian <fabian_carrazan@hotmail.com>
2023-06-30 16:57:41 -0300

    RecyclerView Concat adapter

--- app/src/main/java/com/example/flickfinder/ui/movie/MovieFragment.kt
+++ app/src/main/java/com/example/flickfinder/ui/movie/MovieFragment.kt
@@ -6,17 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.ConcatAdapter
 import com.example.flickfinder.R
 import com.example.flickfinder.core.Resource
+import com.example.flickfinder.data.model.Movie
 import com.example.flickfinder.data.remote.MovieDataSource
 import com.example.flickfinder.databinding.FragmentMovieBinding
 import com.example.flickfinder.presentation.MovieViewModel
 import com.example.flickfinder.presentation.MovieViewModelFactory
 import com.example.flickfinder.repository.MovieRepositoryImpl
 import com.example.flickfinder.repository.RetrofitClient
+import com.example.flickfinder.ui.movie.adapters.concat.MovieAdapter
+import com.example.flickfinder.ui.movie.adapters.concat.PopularConcatAdapter
+import com.example.flickfinder.ui.movie.adapters.concat.TopRatedConcatAdapter
+import com.example.flickfinder.ui.movie.adapters.concat.UpComingConcatAdapter
 
 
-class MovieFragment : Fragment(R.layout.fragment_movie) {
+class MovieFragment : Fragment(R.layout.fragment_movie),MovieAdapter.OnMovieClickListener {
     private lateinit var binding: FragmentMovieBinding
     private val viewModel by viewModels<MovieViewModel> {
         MovieViewModelFactory(
@@ -25,27 +31,41 @@ class MovieFragment : Fragment(R.layout.fragment_movie) {
             )
         )
     }
+    private lateinit var concatAdapter: ConcatAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMovieBinding.bind(view)
 
-        viewModel.fetchMainScreenMovies().observe(viewLifecycleOwner, Observer {result ->
-        when(result){
-            is Resource.Loading ->{
-                Log.d("Livedata","Loading...")
+        concatAdapter = ConcatAdapter()
+
+        viewModel.fetchMainScreenMovies().observe(viewLifecycleOwner, Observer { result ->
+            when (result) {
+                is Resource.Loading -> {
+                    binding.progressBar.visibility = View.VISIBLE
+                }
+                is Resource.Succes -> {
+                    binding.progressBar.visibility = View.GONE
+                    concatAdapter.apply {
+                        addAdapter(0,UpComingConcatAdapter(MovieAdapter(result.data.first.results,this@MovieFragment)))
+                        addAdapter(1, TopRatedConcatAdapter(MovieAdapter(result.data.second.results,this@MovieFragment)))
+                        addAdapter(2, PopularConcatAdapter(MovieAdapter(result.data.third.results,this@MovieFragment))
+                        )
+                    }
+                    binding.rvMovie.adapter = concatAdapter
+                }
+                is Resource.Failure -> {
+                    binding.progressBar.visibility = View.GONE
+
+                    Log.d("Error", "${result.exception}")
+                }
             }
-            is Resource.Succes ->{
-                Log.d("LiveData", "Upcoming: ${result.data.first} ")
-                Log.d("Livedata", "TopRated: ${result.data.second}")
-                Log.d("Livedata", "Popular: ${result.data.third}")
-            }
-            is Resource.Failure -> {
-                Log.d("Error","${result.exception}")
-            }
-        }
 
         })
+    }
+
+    override fun onMovieClick(movie: Movie) {
+        val action = MovieFragmentDirections.
     }
 
 }
